46213ada9082cf7299d3d228982f5f688006090b
Russell <rj.evangelista@hotmail.com>
2021-02-27 15:57:01 +0100

    Check with lbfgs algo

--- main.R
+++ main.R
@@ -13,7 +13,7 @@ J <- c(0.7, 2)
 T_ <- 100
 sims <- 1000
 dof <- 4
-params_studentT <- matrix(nrow = sims, ncol = 5)
+params_studentT100 <- matrix(nrow = sims, ncol = 5)
 for (i in 1:sims) {
   # Simulate Y
   sim <- simulateVar1_studentT(T_, dof, A, J)
@@ -22,24 +22,24 @@ for (i in 1:sims) {
   H <- determine_H(sim$Y_sim)
 
   # Estimate VAR(1) - Nelder-Mead - 1000 iterations
-  estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim$Y_sim, H=H, control=list(maxit=1000))
+  estimation_VAR1_1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim$Y_sim, H=H, method="L-BFGS-B", lower=c(-4,-4,-4,-4), upper=c(4,4,4,4), control=list(maxit=200))
   
   # Add to results matrix for later export
-  params_studentT[i,1:4] <- estimation_VAR1$par
-  params_studentT[i,5] <- estimation_VAR1$convergence
+  params_studentT100[i,1:4] <- estimation_VAR1_1$par
+  params_studentT100[i,5] <- estimation_VAR1_1$convergence
   
   # print i to see progress
   print(i)
 }
 
 # Save results to csv
-write.table(params_studentT, file="results/params_studentT100.csv", sep = ',', row.names = FALSE, col.names = FALSE)
+write.table(params_studentT100, file="results/params_studentT100LBFGSB.csv", sep = ',', row.names = FALSE, col.names = FALSE)
 
 ###### Simulate Y - VAR(1) - student-t errors, T=200, dof=4 ######
 T_ <- 200
 sims <- 1000
 dof <- 4
-params_studentT <- matrix(nrow = sims, ncol = 5)
+params_studentT200 <- matrix(nrow = sims, ncol = 5)
 for (i in 1:sims) {
   # Simulate Y
   sim <- simulateVar1_studentT(T_, dof, A, J)
@@ -48,20 +48,16 @@ for (i in 1:sims) {
   H <- determine_H(sim$Y_sim)
 
   # Estimate VAR(1) - Nelder-Mead - 1000 iterations
-  estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim$Y_sim, H=H, control=list(maxit=1000))
+  estimation_VAR1_2 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim$Y_sim, H=H, method="L-BFGS-B", lower=c(-4,-4,-4,-4,-4,-4,-4,-4), upper=c(4,4,4,4,4,4,4,4), control=list(maxit=200))
   
   # Add to results matrix for later export
-  params_studentT[i,1:4] <- estimation_VAR1$par
-  params_studentT[i,5] <- estimation_VAR1$convergence
+  params_studentT200[i,1:4] <- estimation_VAR1_2$par
+  params_studentT200[i,5] <- estimation_VAR1_2$convergence
   
   # print i to see progress
   print(i)
 }
 
 # Save results to csv
-write.table(params_studentT, file="results/params_studentT200.csv", sep = ',', row.names = FALSE, col.names = FALSE)
-
-
-
-
+write.table(params_studentT200, file="results/params_studentT200LBFGSB.csv", sep = ',', row.names = FALSE, col.names = FALSE)
 
--- randomStuff.R
+++ randomStuff.R
@@ -1,95 +1,138 @@
-
-
-# Y <- read.csv(file='Y_CORN_SOY.csv', header=FALSE, col.names=c("Y1", "Y2"))
-# plot(Y[,2],type = "l", col = "red", xlab = "", ylab = "")
-# lines(Y[,1], type = "l", col = "blue")
 # 
-# acf(Y[,1], 25, pl=TRUE)$acf[,,1]
-# acf(Y[,2], 25, pl=TRUE)$acf[,,1]
-# ccf(Y[,1], Y[,2], 25, pl=TRUE)
-# ccf(Y[,2], Y[,1], 25, pl=TRUE)
+# params100 <- read.csv(file='results/params_studentT100.csv', header=FALSE)
+# params200 <- read.csv(file='results/params_studentT200.csv', header=FALSE)
 # 
 # 
+# plot(density(params200[,1]), xlim=c(-4,4))
+# lines(density(params100[,1]), type = "l", col = "blue")
 # 
-# # VAR(1) estimation with simulated data
-# H <- determine_H(Y)
-# estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=Y, H=10, control=list(maxit=1000))
-# estimation_VAR1
+# plot(density(params200[,2]), xlim=c(-4,4))
+# lines(density(params100[,2]), type = "l", col = "blue")
 # 
-# estimation_VAR2 <- optim(c(0,0,0,0,0,0,0,0), portmanteau_fun_VAR2, Y=Y, H=10, control=list(maxit=1000))
+# plot(density(params200[,3]), xlim=c(-4,4))
+# lines(density(params100[,3]), type = "l", col = "blue")
 # 
-# params <- estimation_VAR2$par
-# phi1 <- rbind(c(params[1], params[2]), c(params[3], params[4]))
-# phi2 <- rbind(c(params[5], params[6]), c(params[7], params[8]))
-# 
-# print(phi2)
-# print(eigen(phi2)$values)
+# plot(density(params200[,4]), xlim=c(-4,4))
+# lines(density(params100[,4]), type = "l", col = "blue")
 # 
 # 
 # 
 # 
 # 
 # 
-# params <- estimation_VAR1$par
-# phi1 <- rbind(c(params[1], params[2]), c(params[3], params[4]))
-# # phi2 <- rbind(c(params[5], params[6]), c(params[7], params[8]))
-# 
-# print(phi1)
-# print(eigen(phi1)$values)
-# 
-# 
-# 
-# 
-# 
-# 
-# # Set seed
-# set.seed(10)
-# 
-# # Read data
-# # Y <- read.csv(file = 'Y_sim_Seed10.csv', header = FALSE, col.names=c("Y1", "Y2"))
-# Y <- read.csv(file='Y_sim.csv', header=FALSE, col.names=c("Y1", "Y2"))
-# H <- determine_H(Y)
-# 
-# # Simulate Y - student-t errors, T=1000, dof=4
-# A <- rbind(c(1, -1), c(0,1))
-# J <- c(0.7, 2)
-# sim1 <- simulateVar1(Y, 1000, 4, A, J)
-# 
-# sim1$Y_star_sim
-# 
-# # Plot simulated data
-# # plot(sim1$Y_star_sim[,1],type = "l", col = "red", xlab = "", ylab = "")
-# # lines(sim1$Y_star_sim[,2], type = "l", col = "blue")
-# 
-# # # VAR(1) estimation with simulated data
-# # H <- determine_H(sim1$Y_star_sim)
-# # estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim1$Y_star_sim, H=H, method="BFGS", control=list(maxit=200))
-# 
-# # estimation_VAR1
-# 
-# # # VAR(2) estimation
-# # estimation_VAR2 <- optim(c(0,0,0,0,0,0,0,0), portmanteau_fun_VAR2, Y=Y, H=H, method = "BFGS", control=list(maxit=1000))
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# # VAR(3) estimation
-# # estimation_VAR3 <- optim(c(0,0,0,0,0,0,0,0,0,0,0,0), portmanteau_fun_VAR3, Y=Y, H=H, method = "BFGS", control=list(maxit=1000))
-# 
-# 
-# # params <- result$par
-# # phi1 <- rbind(c(params[1], params[2]), c(params[3], params[4]))
-# # phi2 <- rbind(c(params[5], params[6]), c(params[7], params[8]))
-# 
-# # print(phi1)
-# # print(eigen(phi1)$values)
+# # plot(density(params100[,1][params100[,1] > -4 &  params100[,1] < 4]), xlim=c(-4,4))
+# # plot(density(params100[,2][params100[,2] > -4 &  params100[,2] < 4]), xlim=c(-4,4))
+# # plot(density(params100[,3][params100[,3] > -4 &  params100[,3] < 4]), xlim=c(-4,4))
+# # plot(density(params100[,4][params100[,4] > -4 &  params100[,4] < 4]), xlim=c(-4,4))
 # # 
-# # print(phi2)
-# # print(eigen(phi2)$values)
-# 
-# 
+# # plot(density(params200[,1][params200[,1] > -4 &  params200[,1] < 4]), xlim=c(-4,4))
+# # lines(density(params100[,1][params100[,1] > -4 &  params100[,1] < 4]), type = "l", col = "blue")
+# # 
+# # plot(density(params200[,2][params200[,2] > -4 &  params200[,2] < 4]), xlim=c(-4,4))
+# # lines(density(params100[,2][params100[,2] > -4 &  params100[,2] < 4]), type = "l", col = "blue")
+# # 
+# # plot(density(params200[,3][params200[,3] > -4 &  params200[,3] < 4]), xlim=c(-4,4))
+# # lines(density(params100[,3][params100[,3] > -4 &  params100[,3] < 4]), type = "l", col = "blue")
+# # 
+# # plot(density(params200[,4][params200[,4] > -4 &  params200[,4] < 4]), xlim=c(-4,4))
+# # lines(density(params100[,4][params100[,4] > -4 &  params100[,4] < 4]), type = "l", col = "blue")
+# # 
+# # 
+# # 
+# # 
+# # 
+# # 
+# # 
+# # # Y <- read.csv(file='Y_CORN_SOY.csv', header=FALSE, col.names=c("Y1", "Y2"))
+# # # plot(Y[,2],type = "l", col = "red", xlab = "", ylab = "")
+# # # lines(Y[,1], type = "l", col = "blue")
+# # # 
+# # # acf(Y[,1], 25, pl=TRUE)$acf[,,1]
+# # # acf(Y[,2], 25, pl=TRUE)$acf[,,1]
+# # # ccf(Y[,1], Y[,2], 25, pl=TRUE)
+# # # ccf(Y[,2], Y[,1], 25, pl=TRUE)
+# # # 
+# # # 
+# # # 
+# # # # VAR(1) estimation with simulated data
+# # # H <- determine_H(Y)
+# # # estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=Y, H=10, control=list(maxit=1000))
+# # # estimation_VAR1
+# # # 
+# # # estimation_VAR2 <- optim(c(0,0,0,0,0,0,0,0), portmanteau_fun_VAR2, Y=Y, H=10, control=list(maxit=1000))
+# # # 
+# # # params <- estimation_VAR2$par
+# # # phi1 <- rbind(c(params[1], params[2]), c(params[3], params[4]))
+# # # phi2 <- rbind(c(params[5], params[6]), c(params[7], params[8]))
+# # # 
+# # # print(phi2)
+# # # print(eigen(phi2)$values)
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # params <- estimation_VAR1$par
+# # # phi1 <- rbind(c(params[1], params[2]), c(params[3], params[4]))
+# # # # phi2 <- rbind(c(params[5], params[6]), c(params[7], params[8]))
+# # # 
+# # # print(phi1)
+# # # print(eigen(phi1)$values)
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # # Set seed
+# # # set.seed(10)
+# # # 
+# # # # Read data
+# # # # Y <- read.csv(file = 'Y_sim_Seed10.csv', header = FALSE, col.names=c("Y1", "Y2"))
+# # # Y <- read.csv(file='Y_sim.csv', header=FALSE, col.names=c("Y1", "Y2"))
+# # # H <- determine_H(Y)
+# # # 
+# # # # Simulate Y - student-t errors, T=1000, dof=4
+# # # A <- rbind(c(1, -1), c(0,1))
+# # # J <- c(0.7, 2)
+# # # sim1 <- simulateVar1(Y, 1000, 4, A, J)
+# # # 
+# # # sim1$Y_star_sim
+# # # 
+# # # # Plot simulated data
+# # # # plot(sim1$Y_star_sim[,1],type = "l", col = "red", xlab = "", ylab = "")
+# # # # lines(sim1$Y_star_sim[,2], type = "l", col = "blue")
+# # # 
+# # # # # VAR(1) estimation with simulated data
+# # # # H <- determine_H(sim1$Y_star_sim)
+# # # # estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim1$Y_star_sim, H=H, method="BFGS", control=list(maxit=200))
+# # # 
+# # # # estimation_VAR1
+# # # 
+# # # # # VAR(2) estimation
+# # # # estimation_VAR2 <- optim(c(0,0,0,0,0,0,0,0), portmanteau_fun_VAR2, Y=Y, H=H, method = "BFGS", control=list(maxit=1000))
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # 
+# # # # VAR(3) estimation
+# # # # estimation_VAR3 <- optim(c(0,0,0,0,0,0,0,0,0,0,0,0), portmanteau_fun_VAR3, Y=Y, H=H, method = "BFGS", control=list(maxit=1000))
+# # # 
+# # # 
+# # # # params <- result$par
+# # # # phi1 <- rbind(c(params[1], params[2]), c(params[3], params[4]))
+# # # # phi2 <- rbind(c(params[5], params[6]), c(params[7], params[8]))
+# # # 
+# # # # print(phi1)
+# # # # print(eigen(phi1)$values)
+# # # # 
+# # # # print(phi2)
+# # # # print(eigen(phi2)$values)
+# # # 
+# # # 
