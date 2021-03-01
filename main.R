7c72cd1b39c3641d92dc552c5db68612e504ebd4
Russell <rj.evangelista@hotmail.com>
2021-03-01 22:32:15 +0100

    New simulation method. Keep seed the same for every sim so pure performance of GCov can be tested

--- main.R
+++ main.R
@@ -2,85 +2,52 @@ setwd("~/Documents/master-thesis-R")
 source("portmanteau_funs.R")
 source("simulate.R")
 
-# Set seed
-set.seed(23)
-
 ########## SIMULATION STUDY VAR(1) ##########
 A <- rbind(c(1, -1), c(0,1))
 J <- c(0.7, 2)
 sims <- 1000
 
 ###### Simulate Y - VAR(1) - student-t errors, T=100, dof=4 ######
-T_ <- 100
-dof <- 4
-params_studentT <- matrix(nrow = sims, ncol = 5)
-for (i in 1:sims) {
-  # Simulate Y
-  sim <- simulateVar1_studentT(T_, dof, A, J)
-  
-  # Dynamic H
-  H <- determine_H(sim$Y_sim)
-
-  # Estimate VAR(1) - Nelder-Mead - 1000 iterations
-  estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim$Y_sim, H=H, control=list(maxit=1000))
-  
-  # Add to results matrix for later export
-  params_studentT[i,1:4] <- estimation_VAR1$par
-  params_studentT[i,5] <- estimation_VAR1$convergence
-  
-  # print i to see progress
-  print(i)
-}
-
-# Save results to csv
-write.table(params_studentT, file="results/params_studentT100.csv", sep = ',', row.names = FALSE, col.names = FALSE)
+source("sim_studentT100.R")
 
 ###### Simulate Y - VAR(1) - student-t errors, T=300, dof=4 ######
-T_ <- 300
-dof <- 4
-params_studentT <- matrix(nrow = sims, ncol = 5)
-for (i in 1:sims) {
-  # Simulate Y
-  sim <- simulateVar1_studentT(T_, dof, A, J)
-  
-  # Dynamic H
-  H <- determine_H(sim$Y_sim)
-
-  # Estimate VAR(1) - Nelder-Mead - 1000 iterations
-  estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim$Y_sim, H=H, control=list(maxit=1000))
-
-  # Add to results matrix for later export
-  params_studentT[i,1:4] <- estimation_VAR1$par
-  params_studentT[i,5] <- estimation_VAR1$convergence
-  
-  # print i to see progress
-  print(i)
-}
-
-# Save results to csv
-write.table(params_studentT, file="results/params_studentT300.csv", sep = ',', row.names = FALSE, col.names = FALSE)
+source("sim_studentT300.R")
 
 ###### Simulate Y - VAR(1) - student-t errors, T=500, dof=4 ######
-T_ <- 500
-dof <- 4
-params_studentT <- matrix(nrow = sims, ncol = 5)
-for (i in 1:sims) {
-  # Simulate Y
-  sim <- simulateVar1_studentT(T_, dof, A, J)
-  
-  # Dynamic H
-  H <- determine_H(sim$Y_sim)
-  
-  # Estimate VAR(1) - Nelder-Mead - 1000 iterations
-  estimation_VAR1 <- optim(c(0,0,0,0), portmanteau_fun_VAR1, Y=sim$Y_sim, H=H, control=list(maxit=1000))
-  
-  # Add to results matrix for later export
-  params_studentT[i,1:4] <- estimation_VAR1$par
-  params_studentT[i,5] <- estimation_VAR1$convergence
-  
-  # print i to see progress
-  print(i)
-}
+source("sim_studentT500.R")
 
-# Save results to csv
-write.table(params_studentT, file="results/params_studentT500.csv", sep = ',', row.names = FALSE, col.names = FALSE)
+###### Simulate Y - VAR(1) - student-t errors, T=100, dof=4 - static H=5 ######
+source("sim_studentT100_staticH5.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=300, dof=4 - static H=5 ######
+source("sim_studentT300_staticH5.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=500, dof=4 - static H=5 ######
+source("sim_studentT500_staticH5.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=100, dof=4 - static H=10 ######
+source("sim_studentT100_staticH10.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=300, dof=4 - static H=10 ######
+source("sim_studentT300_staticH10.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=500, dof=4 - static H=10 ######
+source("sim_studentT500_staticH10.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=100, dof=4 - static H=15 ######
+source("sim_studentT100_staticH15.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=300, dof=4 - static H=15 ######
+source("sim_studentT300_staticH15.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=500, dof=4 - static H=15 ######
+source("sim_studentT500_staticH15.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=100, dof=4 - static H=20 ######
+source("sim_studentT100_staticH20.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=300, dof=4 - static H=20 ######
+source("sim_studentT300_staticH20.R")
+
+###### Simulate Y - VAR(1) - student-t errors, T=500, dof=4 - static H=20 ######
+source("sim_studentT500_staticH20.R")
